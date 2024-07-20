dfffd582c82c138e3da96f440d8a5b03bd34ea28
UNYIME INOH <40715735+Unyimesax@users.noreply.github.com>
2024-07-20 16:52:11 +0000

    Added functions with output

--- main.py
+++ main.py
@@ -12,10 +12,30 @@
 #     print(f"Hello {name}")
 #     print(f"How do you do{name}")
 
-def greet_with(name, location):
-    print(f"Hello {name}, how is {location} today?")
+# def greet_with(name, location):
+#     print(f"Hello {name}, how is {location} today?")
 
-greet_with("Anna", "Huntingdon" )
+# greet_with("Anna", "Huntingdon" )
 
-#Functions with Keyword arguments
-greet_with(location="Huntingdon", name="Anna",)
+# #Functions with Keyword arguments
+# greet_with(location="Huntingdon", name="Anna",)
+
+# FUNCTIONS WITH OUTPUTS
+
+
+# def format_name(f_name, l_name):
+#     strip_f = f_name.lower()
+#     strip_l = l_name.lower()
+#     cap_first = strip_f[0].upper()
+#     cap_last = strip_l[0].upper()
+#     final_first = cap_first + strip_f[1:]
+#     final_last = cap_last + strip_l[1:]
+#     print(f"{final_first} {final_last}")
+
+def format_name(f_name, l_name):
+    formatted_fname = f_name.title()
+    formatted_lname = l_name.title()
+    return f"{formatted_fname} {formatted_lname}"
+
+formatted_name = format_name("frEd", "iNOh")
+print(formatted_name)
